f69dc2ce5391123a93c416affb9927a93ae41bf7
Andrii Zavorotnii <andrii.zavorotnii@gmail.com>
2018-01-15 07:13:45 -0800

    Add BSD 2-clause licence

--- main.go
+++ main.go
@@ -1,3 +1,5 @@
+// Copyright 2018 Andrii Zavorotnii. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 package main
 
 import (
